872a3eab4640092283706b713f334418f347fbeb
reeuwijk <CvR42@users.noreply.github.com>
1997-07-16 10:43:27 +0000

    Createstring added.

--- tmkernel/libtmc/lnt2s.c
+++ tmkernel/libtmc/lnt2s.c
@@ -13,16 +13,25 @@
 tmstring tmtext_to_tmstring_lognew( const tmtext t, const char *file, const int line )
 {
     size_t sz;
-    long ix;
     tmstring s;
 
     sz = (size_t) t->sz;
-    s = new_tmstring_lognew( "", file, line );
-    /* Allocate room for the tmstring and the terminating '\0'. */
-    s = realloc_tmstring_lognew( s, sz+1, file, line );
-    for( ix=0; ix<t->sz; ix++ ){
-	s[ix] = t->arr[ix];
+    /* Create a string big enough to hold the text and the terminating '\0'. */
+    s = create_tmstring_lognew( sz+1, file, line );
+    if( s == tmstringNIL ){
+	return s;
     }
+#if HAVE_MEMMOVE
+    memcpy( s, t->arr, sz );
+#else
+    {
+	long ix;
+
+	for( ix=0; ix<t->sz; ix++ ){
+	    s[ix] = t->arr[ix];
+	}
+    }
+#endif
     s[sz] = '\0';
     return s;
 }
