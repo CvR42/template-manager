4f687cbb287f85184acabb493c1754f675c18148
reeuwijk <reeuwijk@4e265e03-a60f-0410-8690-a5dcfc437203>
1997-09-10 09:26:06 +0000

    Fixed bug in buffer handling of fscan_word routines. Removed fixed buffer size in string fscan_ and print_ routines.

--- trunk/tmkernel/libtmc/lnfscwrd.c
+++ trunk/tmkernel/libtmc/lnfscwrd.c
@@ -4,10 +4,10 @@
  * All rights reserved.
  */
 
-/* File: fscstr.c
+/* File: lnfscwrd.c
  * 
  * Handle 'fscan_<type>' for type 'tmword'.
- * Version for use without lognew.
+ * Version for use with lognew.
  */
 
 #include <ctype.h>
@@ -15,43 +15,44 @@
 #include "tmc.h"
 
 /* Try to read a tmword in the buffer 'buf'. Give an error
-   message if this is not successful. A tmword may contain
-   escape sequences with a '\', but no newlines. The '"'
-   around the tmword are stripped.
+   message if this is not successful. 
  */
 int fscan_tmword_lognew( FILE *f, tmword *s, const char *file, const int line )
 {
     int c;
     int brac;
-    unsigned int ix = 0;
-    unsigned int sz = 4;
-    tmstring buf = create_tmstring_lognew( sz, file, line );
 
     *s = tmwordNIL;
     brac = tm_fscanopenbrac( f );
     c = fgetc( f );
     if( c != '@' ){
+	unsigned int sz = INITIAL_STRINGSIZE;
+	tmstring buf = create_tmstring_lognew( sz, file, line );
+	unsigned int ix = 0;
+
 	for(;;){
-	    if( isspace( c ) || strchr( "(),@[]", c ) != NULL ){
+	    if( isspace( c ) || strchr( "()|,@[]", c ) != NULL ){
                 if( ix == 0 ){
                     (void) sprintf(
                         tm_errmsg,
                         "tmword expected, but got ascii code 0x%02x",
                         (c & 0xff) 
                     );
+		    fre_tmstring_lognew( buf );
                     return 1;
                 }
 		ungetc( c, f );
 		break;
 	    }
 	    if( ix+1>sz ){
-		buf = realloc_tmstring_lognew( buf, sz+sz, file, line );
+		sz += sz+1;
+		buf = realloc_tmstring_lognew( buf, sz, file, line );
 	    }
 	    buf[ix++] = c;
 	    c = fgetc( f );
 	}
+	buf[ix] = '\0';
+	*s = buf;
     }
-    buf[ix] = '\0';
-    *s = buf;
     return tm_fscanclosebrac( f, brac );
 }
