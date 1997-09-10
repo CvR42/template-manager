7b7524ed1fd3922042955fd029e70c71d773c0a2
reeuwijk <CvR42@users.noreply.github.com>
1997-09-10 09:26:06 +0000

    Fixed bug in buffer handling of fscan_word routines. Removed fixed buffer size in string fscan_ and print_ routines.

--- trunk/tmkernel/libtmc/prstr.c
+++ trunk/tmkernel/libtmc/prstr.c
@@ -11,37 +11,43 @@
 #include "config.h"
 #include "tmc.h"
 
-#define BACKSLASH '\\'
-
 /* definition of 'print' for a 'tmstring' */
 void print_tmstring( TMPRINTSTATE *st, tmconststring s )
 {
-    char buf[STRBUFSZ+10];
-    char *bufp;
-    char *bufend;
-    unsigned int c;
+    tmstring buf;
     const char *sp;
-    const char *pp;
+    unsigned int sz;
+    unsigned int ix = 0;
 
     if( s == tmstringNIL ){
 	tm_printword( st, "@" );
 	return;
     }
+    sz = strlen( s )+20;
+    buf = create_tmstring_nolognew( sz );
     sp = s;
-    bufp = buf;
-    bufend = &buf[STRBUFSZ];
-    *bufp++ = '"';
+    buf[ix++] = '"';
     while( *sp != '\0' ){
+	unsigned int esz;
+	const char *pp;
+	unsigned int c;
+
 	c = ((unsigned int) *sp++) & 0xff;
 	pp = tm_escapestring( c );
-	while( *pp != '\0' && bufp<bufend ){
-	    *bufp++ = *pp++;
+	esz = strlen( pp );
+	if( ix+esz+2>=sz ){
+	    sz += TEXTGROWSTEP+2+esz;
+	    buf = realloc_tmstring_nolognew( buf, sz );
 	}
-	if( bufp>=bufend ){
-	    tm_fatal( __FILE__, __LINE__, "tmstring too long" );
+	while( *pp != '\0' ){
+	    buf[ix++] = *pp++;
 	}
     }
-    *bufp++ = '"';
-    *bufp = '\0';
+    /* Since we've ensured that there is always room for these two
+     * characters, we can blindly write into the buffer.
+     */
+    buf[ix++] = '"';
+    buf[ix] = '\0';
     tm_printword( st, buf );
+    fre_tmstring_nolognew( buf );
 }
