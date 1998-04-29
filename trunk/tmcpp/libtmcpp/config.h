b15fd0735c7bad949feaf92d28e7d056cab1b75b
reeuwijk <CvR42@users.noreply.github.com>
1998-04-29 17:35:02 +0000

    Added tmtext::append( const tmtext * ) and tmtext::append( const char * ) and test routines.

--- trunk/tmcpp/libtmcpp/config.h
+++ trunk/tmcpp/libtmcpp/config.h
@@ -1,15 +1,13 @@
+/* config.h.  Generated automatically by configure.  */
 /* Tm - an interface code generator.
  * Author: C. van Reeuwijk.
  *
  * All rights reserved.
  */
 
-/* File: config.h
- *
- * Some configuration constants.
- * This version is specific for Borland C++ for Windows. 
- */
-#define FIRSTLOGNEWSZ 100	/* First table size. */
+// File: config.h
+//
+// Some configuration constants.
 #define HASHBITS 6		/* Number of bits used in hashing. */
 #define HASHSHIFT 2		/* Number of bits to skip in hashing key. */
 #define TEXTGROWSTEP 40		/* The growth step of the text putc routine */
@@ -17,9 +15,14 @@
 
 #define SYMHASHWIDTH 1024
 
-#define HAVE_STRERROR 0
-#define HAVE_MEMMOVE 0
+/* #undef const */
+#define STDC_HEADERS 1
+#define HAVE_STRERROR 1
+#define HAVE_MEMMOVE 1
 
-#include <stdio.h>
-#include <stdlib.h>
+#ifdef __GNUC__
+#define __USE_FIXED_PROTOTYPES__
+#endif
+
 #include <string.h>
+
