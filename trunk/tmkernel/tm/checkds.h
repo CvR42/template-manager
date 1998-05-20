462eddcf97adc7d1c6b11ca2d233e798a2b39346
reeuwijk <CvR42@users.noreply.github.com>
1998-05-20 14:25:12 +0000

    Cosmetic changes. Fixed a bug that caused a crash with .ds files with double field names.

--- trunk/tmkernel/tm/checkds.h
+++ trunk/tmkernel/tm/checkds.h
@@ -1,4 +1,4 @@
 /* File: checkds.h */
 
 extern bool check_ds_list( const ds_list dl );
-extern bool cktuple( tmstring nm, Field_list fields, tmstring_list inherits );
+extern void cktuple( tmstring nm, Field_list fields, tmstring_list inherits );
